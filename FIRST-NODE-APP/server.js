673b4d9d458769cdf1882679a33d2860335e402d
rohnshrma <rohn11shrma@gmail.com>
2026-03-11 15:20:37 +0530

    feat: Add profile submission form and update contact route handling

--- FIRST-NODE-APP/server.js
+++ FIRST-NODE-APP/server.js
@@ -5,10 +5,14 @@ const app = express();
 
 const PORT = 3000;
 
+const profiles = [];
+
 //
 app.use(morgan("dev"));
 
 app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 
@@ -18,16 +22,24 @@ app.route("/").get((req, res) => {
   res.sendFile(`${cwd()}/pages/index.html`);
 });
 
+// about
 app.route("/about").get((req, res) => {
   res.sendFile(`${cwd()}/pages/about.html`);
 });
 
-app.route("/contact").get((req, res) => {
-  res.sendFile(`${cwd()}/pages/contact.html`);
-});
+// contact
+app
+  .route("/contact")
+  .get((req, res) => {
+    res.sendFile(`${cwd()}/pages/contact.html`);
+  })
+  .post((req, res) => {
+    profiles.push(req.body);
+    res.send("<div>Prfile Added</div><br><a href='/'>Go Back</a>");
+  });
 
-app.use((req, res) => {
-  res.status(404).sendFile(`${cwd()}/pages/404.html`);
-});
+// app.use((req, res) => {
+//   res.status(404).sendFile(`${cwd()}/pages/404.html`);
+// });
 
 app.listen(PORT, () => console.log("Server started on port : ", PORT));
